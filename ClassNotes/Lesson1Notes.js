dca8996b0e63160b86df4d7ea3dc6251bfde7cea
rishigujadhur1 <142503662+rishigujadhur1@users.noreply.github.com>
2023-09-17 12:55:30 +0400

    Class Notes

---
+++ ClassNotes/Lesson1Notes.js
@@ -0,0 +1,42 @@
+// JS class notes - 16/09/23
+
+//Let vs Const variable declaration
+let A = 1; A = 2;
+const B = 1;
+
+//Functions:
+function myFunc(){}
+ 
+// Parameterized functions
+function myFunc(a,b){}
+
+// Function expressions
+let myFunc = function() {}
+
+// Arrow Functions 
+let myFunction = () => {}
+
+// Modules - Import/Export functions
+// "Type": "Module" in Package.json 
+// Export function or variable (in one fule)
+// Import {name} from 'path' (in another file)
+
+//Package.json overview - 
+// It contains metadata about the project
+// (like the project name and description) 
+// and a list of dependencies 
+// required by the application.
+
+// Node.js overview
+// Node.js is a back-end JavaScript runtime environment,
+// runs on the V8 JavaScript engine,
+// and executes JavaScript code outside a web browser. 
+
+// Shortcut for HTML formatting in VS Code
+// CTRL + K CTRL + F
+
+//HTML
+//<html><head><title>Name</title></head><body></body></html>
+
+// Script referencing js file <script src="path"></script>
+// Script for js modules <script type="module" src="path"></script>
