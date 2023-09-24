c27be532931f041de158be9167e6405a30ec20df
rishigujadhur1 <142503662+rishigujadhur1@users.noreply.github.com>
2023-09-25 00:10:39 +0400

    Todo App

---
+++ Projects/TodoApp/main.js
@@ -0,0 +1,26 @@
+const inputBox = document.querySelector("#input-box");
+const listContainer = document.querySelector("#list-container");
+
+function addTask() {
+  let li = document.createElement("li");
+  li.className = "list-group-item";
+  li.innerHTML = inputBox.value;
+  listContainer.appendChild(li);
+
+  let span = document.createElement("span");
+  span.className = "btn p-2 btn-danger ms-5";
+  span.innerHTML = "-";
+  li.appendChild(span);
+
+  inputBox.value = "";
+}
+
+listContainer.addEventListener(
+  "click",
+  function (e) {
+    if (e.target.tagName === "SPAN") {
+      e.target.parentElement.remove();
+    }
+  },
+  false
+);
