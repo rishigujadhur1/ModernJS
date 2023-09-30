a0e4fab2e27fae91186b48ab118e8f019b58b8fa
rishigujadhur1 <142503662+rishigujadhur1@users.noreply.github.com>
2023-09-30 16:06:29 +0400

    challenge projects

---
+++ Activities/challenges/challenge 2/challenge.js
@@ -0,0 +1,33 @@
+// Your challenge is to replicate the Instagram User Interface as closely as possible,
+// emphasizing your proficiency in HTML, CSS, and JavaScript.
+
+// Key Objectives:
+// 1. Recreate the Instagram UI with a focus on responsiveness.
+// 2. Implement core functionalities and interactions to mimic the Instagram experience.
+
+// Your final project should encompass the following key components:
+
+// 1. Home Page:
+//    - Develop a fully functional Instagram home page that closely resembles the original design.
+//    - Ensure seamless navigation and interaction with posts and the user feed.
+//    - Incorporate the header and footer to maintain consistency.
+
+// 2. User Profile Page:
+//    - Create user profile pages accessible through the URL '/profile/{username}'.
+//    - Display user-specific content, including posts, followers, and following.
+
+// 3. Explore Page:
+//    - Develop an 'Explore' page, accessible via the URL '/explore'.
+//    - Populate this page with posts
+//    - Enable users to search and discover new content.
+
+// 4. Responsive Design:
+//    - Ensure your Instagram clone is responsive, adapting to various screen sizes and devices.
+//    - Prioritize mobile responsiveness for a seamless mobile experience.
+
+// 5. Additional Features (Optional):
+//    - Consider adding extra features such as direct messaging, notifications, or user interactions.
+//    - Enhance the user experience with creative animations and transitions.
+
+// Remember to pay attention to detail, including typography, color schemes, and user interactions,
+// to ensure your Instagram clone closely resembles the original application.
---
+++ Activities/challenges/challenge 2/notes.md
@@ -0,0 +1,28 @@
+# Instagram UI Clone Project
+
+## Project Summary
+
+### Home Page
+
+Ensure that the Home page includes both the header and footer.
+
+### Profile Page
+
+The profile page should be accessible via the URL /profile/{username}
+
+### Explore Page
+
+The explore page should be accessible via the URL /explore.
+
+### Post Page
+
+Individual post pages should be accessible via /post/{post-id}.
+
+## Instructions
+
+Your challenge is to develop this project and make it resemble the Instagram design as closely as possible.
+
+Your project should:
+
+1. Be responsive, ensuring it functions smoothly on both desktop and mobile devices.
+2. Implement the minimum required functionalities and effects specified in the design.
