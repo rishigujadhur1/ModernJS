b8c894fc90a1540393a8a5aa2fa81826193c133f
rishigujadhur1 <142503662+rishigujadhur1@users.noreply.github.com>
2023-10-04 13:07:15 +0400

    Reverted changes to quote project file

--- Projects/QuoteGenerator/main.js
+++ Projects/QuoteGenerator/main.js
@@ -1,22 +1,17 @@
-//1. Create an array called colors and add three color names to it. ["red", "blue", "yellow"]
-// (){} for functions , [] for arrays, {} for objects
-// .map(()=> ) , callback is a call to another function from 1 function
-// all keywords which are not from JS are variables
-// string = "abc" or 'abc'
+const author = document.querySelector("#author");
+const quote = document.querySelector(".blockquote");
 
-const colors= ["red","blue","yellow"];
-//2. Declare an array numbers with the numbers 1 through 5 in it.
-var Number= [1,2,3,4,5];
-//3. Define a function that accepts an array of numbers and returns the sum of
-// all numbers in the array.
-
-
-function sum(Num1){
-  // declare variable total
-  let total=0;
-  // add value to total on each loop with: total += value
-  Num1.map((value)=>total = total + value);
-  console.log(total);
+async function GetQuote() {
+  debugger;
+  const response = await fetch("http://api.quotable.io/random");
+  let data = await response.json();
+  quote.innerHTML = data.content;
+  author.innerHTML = data.author;
 }
 
-sum([1,2,3,4,5]);
+GetQuote();
+
+const btn1 = document.querySelector(".btn");
+btn1.addEventListener("click", () => {
+  GetQuote();
+});
