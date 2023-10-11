e51a7a815763e74efef12fde1cd1072305e544db
rishigujadhur1 <142503662+rishigujadhur1@users.noreply.github.com>
2023-10-11 16:01:53 +0400

    BS5 project + Slides + js interview questions

---
+++ Activities/interview questions/questions/questions.js
@@ -0,0 +1,39 @@
+// What is JavaScript, and how does it differ from Java?
+
+// How do you declare a variable in JavaScript?
+
+// Explain the difference between let, const, and var for variable declaration.
+
+// What is the significance of "hoisting" in JavaScript?
+
+// How do you check the data type of a variable in JavaScript?
+
+// Explain the concept of "closures" in JavaScript.
+
+// What is the difference between "null" and "undefined" in JavaScript?
+
+// How does the "this" keyword work in JavaScript?
+
+// What is an IIFE (Immediately Invoked Function Expression) in JavaScript, and why is it used?
+
+// Explain the concept of "callback functions" and provide an example of their use.
+
+// What is an "arrow function" in JavaScript, and how does it differ from regular functions?
+
+// What are "promises" in JavaScript, and how do they work?
+
+// What is "asynchronous programming," and how is it achieved in JavaScript?
+
+// What is the purpose of the "localStorage" and "sessionStorage" objects in the browser?
+
+// Explain the difference between "null" and "undefined."
+
+// What is the "DOM" in the context of web development and JavaScript?
+
+// How do you handle errors in JavaScript, and what are the types of errors you might encounter?
+
+// Explain the use of "JSON" in JavaScript and how you can parse and stringify JSON data.
+
+// What is a "closure" in JavaScript, and why might you use one?
+
+// What is the "event delegation" pattern in JavaScript, and why is it useful in web development?
